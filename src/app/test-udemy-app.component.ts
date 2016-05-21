368187ec98c6f7b3691592018fed5a396dff2944
Alex Carlson <alxcrlsn@Alexs-MBP.domain>
2016-05-21 12:11:08 -0600

    Initial Commit

--- src/app/test-udemy-app.component.ts
+++ src/app/test-udemy-app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CoursesComponent } from './course.component'
+import { AuthorComponent } from './author.component'
 
 @Component({
   moduleId: module.id,
   selector: 'test-udemy-app-app',
-  templateUrl: 'test-udemy-app.component.html',
-  styleUrls: ['test-udemy-app.component.css']
+  template: `
+  <h1>Welcome!</h1>
+  <courses></courses>
+  <authors></authors>
+  `,
+  styleUrls: ['test-udemy-app.component.css'],
+  directives: [CoursesComponent, AuthorComponent]
 })
 export class TestUdemyAppAppComponent {
-  title = 'test-udemy-app works!';
+  title: "test-udemy-app works!"
 }
